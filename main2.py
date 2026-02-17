62ef68be462165bdc3d510dbc8554daff30f0f66
Alexander Pronin <a.pronin@m3ii.tech>
2026-02-17 21:01:04 +0300

    f

--- main2.py
+++ main2.py
@@ -1,27 +1,75 @@
-from src.employee import Developer, SystemAnalyst
+from typing import TYPE_CHECKING
+
+from src.employee import Developer, SystemAnalyst, QA
 from src.feature import Feature, FeatureStage
+
+
+if TYPE_CHECKING:
+    from src.employee import Employee
+
 
 # team
 igor = Developer(name ='Igor')
-
 daniil = SystemAnalyst(name ='Daniil')
+roman = QA(name ='Roman')
 
 # features
 bepaid_integration = Feature(
-    name = 'Bepaid Integration',
+    name = 'BePaid Integration',
     capacity_mapping = {
         FeatureStage.ANALYTICS: 3,
         FeatureStage.DEVELOPMENT: 3,
+        FeatureStage.TESTING: 3,
     },
     current_stage = FeatureStage.ANALYTICS,
+)
+
+bank131_integration = Feature(
+    name = 'Bank 131 Integration',
+    capacity_mapping = {
+        FeatureStage.ANALYTICS: 3,
+        FeatureStage.DEVELOPMENT: 3,
+        FeatureStage.TESTING: 3,
+    },
+    current_stage = FeatureStage.DEVELOPMENT,
 )
 
 # planning
 bepaid_integration.assign(igor)
 bepaid_integration.assign(daniil)
+bepaid_integration.assign(roman)
 
-EMPLOYEES = [igor, daniil]
-FEATURES = [bepaid_integration]
+bank131_integration.assign(igor)
+bank131_integration.assign(daniil)
+bank131_integration.assign(roman)
+
+EMPLOYEES = [igor, daniil, roman]
+FEATURES = [bepaid_integration, bank131_integration]
+
+def calc_employee_day(employee: 'Employee') -> None:
+    for feature in FEATURES:
+        if employee in feature.assignees:
+            if feature.current_stage in employee.effective_stages:
+                employee.work(feature)
+
+                if feature.capacity_mapping[feature.current_stage] == 0:
+                    feature.capacity_mapping.pop(feature.current_stage)
+                    print(f'Finished `{feature.name}` {feature.current_stage.name}')
+
+                    remain_stages = list(feature.capacity_mapping.keys())
+                    if len(remain_stages) == 0:
+                        print(f'Feature `{feature.name}` is done!')
+                        FEATURES.remove(feature)
+
+                    feature.current_stage = list(feature.capacity_mapping.keys())[0]
+                return
+
+
+            # else:
+            #     print(f'{employee.name} cant\'t work on `{feature.name}`')
+            # employee.idle()
+
+
 
 if __name__ == '__main__':
     for day_number in range(1, 15):
@@ -30,13 +78,5 @@ if __name__ == '__main__':
             print('All done!')
             break
         for employee in EMPLOYEES:
-                for feature in FEATURES:
-                    if employee in feature.assignees:
-                        if feature.current_stage in employee.effective_stages:
-                            finished = employee.work(feature)
-                            if finished:
-                                FEATURES.remove(feature)
-                        else:
-                            employee.idle()
-
+            calc_employee_day(employee)
         print('------------------')
--- src/employee.py
+++ src/employee.py
@@ -4,23 +4,15 @@ from src.feature import Feature, FeatureStage
 class Employee:
 
     day_work_hours = 7
+    effective_stages: list[FeatureStage]
 
     def __init__(self, name: str):
         self.name = name
 
     def work(self, feature: 'Feature') -> bool | None:
-        print(f'{self.name} works on {feature.name}')
+        print(f'{self.name} doing {feature.current_stage.name} of `{feature.name}`')
         feature.capacity_mapping[feature.current_stage] -= 1
-        if feature.capacity_mapping[feature.current_stage] == 0:
-            finished_stage = feature.capacity_mapping.pop(feature.current_stage)
-            print(f'Finished stage {feature.current_stage.name}')
 
-            remain_stages = list(feature.capacity_mapping.keys())
-            if len(remain_stages) == 0:
-                print(f'Feature `{feature.name}` is done!')
-                return True
-
-            feature.current_stage = list(feature.capacity_mapping.keys())[0]
 
     def idle(self) -> None:
         print(f'{self.name} idle')
@@ -32,3 +24,7 @@ class Developer(Employee):
 
 class SystemAnalyst(Employee):
     effective_stages = [FeatureStage.ANALYTICS]
+
+
+class QA(Employee):
+    effective_stages = [FeatureStage.TESTING]
