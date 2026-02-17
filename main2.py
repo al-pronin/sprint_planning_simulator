29a3be8ddcd2aa23ef511fcd0138b9bd9539732c
Alexander Pronin <a.pronin@m3ii.tech>
2026-02-17 20:35:49 +0300

    f

--- main2.py
+++ main2.py
@@ -24,12 +24,19 @@ EMPLOYEES = [igor, daniil]
 FEATURES = [bepaid_integration]
 
 if __name__ == '__main__':
-    for day_number in range(14):
+    for day_number in range(1, 15):
         print(f'Day {day_number}')
+        if not FEATURES:
+            print('All done!')
+            break
         for employee in EMPLOYEES:
-            for feature in FEATURES:
-                if employee in feature.assignees:
-                    if feature.current_stage in employee.effective_stages:
-                        employee.work(feature)
-                    else:
-                        employee.idle()
+                for feature in FEATURES:
+                    if employee in feature.assignees:
+                        if feature.current_stage in employee.effective_stages:
+                            finished = employee.work(feature)
+                            if finished:
+                                FEATURES.remove(feature)
+                        else:
+                            employee.idle()
+
+        print('------------------')
--- src/employee.py
+++ src/employee.py
@@ -8,10 +8,19 @@ class Employee:
     def __init__(self, name: str):
         self.name = name
 
-    def work(self, feature: 'Feature') -> None:
+    def work(self, feature: 'Feature') -> bool | None:
         print(f'{self.name} works on {feature.name}')
         feature.capacity_mapping[feature.current_stage] -= 1
-        print(f'Feature capacity: {feature.capacity_mapping[feature.current_stage]}')
+        if feature.capacity_mapping[feature.current_stage] == 0:
+            finished_stage = feature.capacity_mapping.pop(feature.current_stage)
+            print(f'Finished stage {feature.current_stage.name}')
+
+            remain_stages = list(feature.capacity_mapping.keys())
+            if len(remain_stages) == 0:
+                print(f'Feature `{feature.name}` is done!')
+                return True
+
+            feature.current_stage = list(feature.capacity_mapping.keys())[0]
 
     def idle(self) -> None:
         print(f'{self.name} idle')
